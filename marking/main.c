4b5c11d661f0ba020ef388ef0ce6e9485a10858e
20161104569 <a201611104569@admindeimac-2.local>
2018-06-25 10:49:44 +0800

    Initial Commit

---
+++ marking/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  marking
+//
+//  Created by 20161104569 on 18/6/25.
+//  Copyright © 2018年 20161104569. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
