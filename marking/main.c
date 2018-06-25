4b26da058a7a62351ab7e0e98203af8f75fb0236
20161104569 <2174037646@qq.com>
2018-06-25 11:23:23 +0800

    marking
    裁判对演员节目打分

--- marking/main.c
+++ marking/main.c
@@ -7,9 +7,34 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#define N 4
+void main()
+{
+    int a[N]={0};
+    int i;
+    int max,min,sum;
+    sum=0;
+    for(i=0;i<N;i++)
+    {
+        printf("请输入裁判要给的分数:");
+        scanf("%d",&a[i]);
+        if(a[i]<0||a[i]>100)
+        {
+            printf("输入有误,请重新输入:");
+            scanf("%d",&a[i]);
+        }
+    }
+    min=a[0];
+    max=a[0];
+    for(i=0;i<N;i++)
+    {
+        if(min>a[i])
+            min=a[i];
+        if(max<a[i])
+        max=a[i];
+        sum=sum+a[i];
+        
+    }
+    printf("该选手的平均分为: %d",(sum-max-min)/(N-2));
 }
+
